2ac0b6657c24eba4d5ebfdc72244702ba3b44c9b
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2024-10-29 22:47:29 +0300

    Update Wiegand.h

--- src/Wiegand.h
+++ src/Wiegand.h
@@ -1,35 +1,28 @@
-#ifndef _WIEGAND_H
-#define _WIEGAND_H
-
-#if defined(ARDUINO) && ARDUINO >= 100
+#pragma once
 #include "Arduino.h"
+#if defined(ESP8266)
+#define INTERRUPT_ATTR ICACHE_RAM_ATTR
+#elif defined(ESP32)
+#define INTERRUPT_ATTR IRAM_ATTR
 #else
-#include "WProgram.h"
+#define INTERRUPT_ATTR
 #endif
 
 class WIEGAND {
 
 public:
-	WIEGAND();
-	void begin();
-	void begin(int pinD0, int pinD1);
-	void begin(int pinD0, int pinIntD0, int pinD1, int pinIntD1);
-	bool available();
-	unsigned long getCode();
-	int getWiegandType();
-	
+	WIEGAND() { WIEGAND(2, 3);}
+	WIEGAND(byte pinD0, byte pinD1);
+	uint32_t getCode() { return _card; };
+	byte getWiegandType() { return _wiegandType; };
+	bool DoWiegandConversion();
 private:
-	static void ReadD0();
-	static void ReadD1();
-	static bool DoWiegandConversion ();
-	static unsigned long GetCardId (volatile unsigned long *codehigh, volatile unsigned long *codelow, char bitlength);
-	
-	static volatile unsigned long 	_cardTempHigh;
-	static volatile unsigned long 	_cardTemp;
-	static volatile unsigned long 	_lastWiegand;
-	static volatile int				_bitCount;	
-	static int				_wiegandType;
-	static unsigned long	_code;
+	static void ReadD0() INTERRUPT_ATTR;
+	static void ReadD1() INTERRUPT_ATTR;
+	uint32_t GetCardId();
+	static volatile uint64_t	_bitstream;
+	static volatile uint32_t 	_lastWiegand;
+	static volatile byte		_bitCount;
+	byte						_wiegandType;
+	uint32_t 					_card;
 };
-
-#endif
