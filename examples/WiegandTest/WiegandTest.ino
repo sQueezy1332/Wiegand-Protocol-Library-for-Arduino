571aba003f5860088c808b4c1e949838fb96294e
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2024-10-29 22:49:16 +0300

    Update WiegandTest.ino

--- examples/WiegandTest/WiegandTest.ino
+++ examples/WiegandTest/WiegandTest.ino
@@ -3,16 +3,15 @@
 WIEGAND wg;
 
 void setup() {
-	Serial.begin(9600);  
+	Serial.begin(115200);  
 	
 	// default Wiegand Pin 2 and Pin 3 see image on README.md
 	// for non UNO board, use wg.begin(pinD0, pinD1) where pinD0 and pinD1 
 	// are the pins connected to D0 and D1 of wiegand reader respectively.
-	wg.begin();
 }
 
 void loop() {
-	if(wg.available())
+	while(!wg.available())
 	{
 		Serial.print("Wiegand HEX = ");
 		Serial.print(wg.getCode(),HEX);
